625eb22cb8b50841f281f4b2a27ca13c1e485812
aleqsandremargiani <aleqsandremargiani.230@gmail.com>
2024-10-13 02:13:05 -0700

    first commit

---
+++ day01/homework/homework.py
@@ -0,0 +1,32 @@
+# name არის ცვლადი
+# = არის ცვლადისღვის მიმნიჭებელი სიმბოლო
+#"nika" არის ცვლადისთვის მნიშვნელობა
+
+surname="margiani"
+
+#print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name ="aleqsandre"  #ეს არის str (string) ტიპის ცვლადი
+age = 13 # ეს არის int (integer) მთელი რიცხვი
+height= 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#blome (bool) ტიპის ცვლადი
+
+knows_programing = True # true ან false
+is_ugly = False #snakecase(უბრალოდ წერსი სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+print(name + " " + surname)
+# სტრინგი არიდ ბრჭყალებში მოქცეული სიმბოლოები
+print(name + "age")
+
+print(name + surname + "age" )
+
+print(type(age))
+
+
+
+
+print(name + " "+ surname )
